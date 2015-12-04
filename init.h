df476549e501480cf78cb1ef9554638a398e2ae5
Hujjo <hugo.bernet@hotmail.fr>
2015-12-04 01:56:48 +0100

    Correction of false deleting of Fahed

---
+++ init.h
@@ -0,0 +1,12 @@
+#ifndef INIT_H_INCLUDED
+#define INIT_H_INCLUDED
+
+#define TRUE 1
+#define FALSE 0
+
+#define PUBLIC 1
+#define PRIVATE 0
+
+// todo : define a new type : boolean
+
+#endif INIT_H_INCLUDED
